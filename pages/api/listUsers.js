9f08062ed98cf273a533d9fbf01d3159e4c1e6da
David Rodriguez Gonzalez <davidrguez_29@hotmail.com>
2022-11-29 23:42:51 -0600

    Se agrega el endpoint listUsers

---
+++ pages/api/listUsers.js
@@ -0,0 +1,31 @@
+import { Octokit } from '@octokit/core';
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+export default async (req, res) => {
+  try {
+    const since = req.query.since || 0;
+    const per_page = req.query.per_page || 10;
+    const response = await octokit.request(`GET /users?since=${since}&per_page=${per_page}`, {
+      headers: {
+        accept: 'application/vnd.github+json',
+      },
+    });
+    res.json({
+      data: response.data.map((user) => ({
+        id: user.id,
+        avatar: user.avatar_url,
+        username: user.login,
+        github: user.html_url,
+      })),
+      status: response.status,
+    });
+  } catch (err) {
+    res.json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+};
